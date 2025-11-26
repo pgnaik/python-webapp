1e1039c6c7202de97d655d0382b0c1936bd91a12
Dr. Poornima G. Naik <31154391+pgnaik@users.noreply.github.com>
2025-11-26 16:06:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello from CI/CD pipeline using Git, Docker, and Jenkins!"
+    return "<h1>Hello from <font color='green'>CI/CD pipeline</font> using <font color='blue'>Git, Docker, and Jenkins!</font></h1>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
